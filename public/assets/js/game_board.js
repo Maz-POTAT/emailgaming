8d7c409bdcc415075261008c4e51f5beeb1817ac
tuktarov9898 <84121020+Maz-POTAT@users.noreply.github.com>
2021-06-03 18:37:16 +0300

    invite validation

--- public/assets/js/game_board.js
+++ public/assets/js/game_board.js
@@ -1,4 +1,9 @@
 ﻿var user_role = -1;
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+  
 $(document).ready(function () {
     $('body').on('click', '#user_role_1', function(){
         if($('#user_role_1').hasClass('active')){
@@ -26,8 +31,51 @@ $(document).ready(function () {
         }
     })
 
-    $('body').on('click', '#user_invite', function(){
-        
+    $('body').on('click', '#start_room', function(){
+        if (validateEmail($('#oppo_email').val())) {
+            let bSuccess = true;
+            if (validateEmail($('#my_email').val())) {
+                $('#alert_area_my_email').html('');
+            } else{
+                $('#alert_area_my_email').html('<div class="alert alert-danger alert-dismissable">\
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>\
+                    Please insert a vaild email address.\
+                </div>');
+                bSuccess = false;
+            }
+
+            if (!$('#game_title').val().length == 0) {
+                $('#alert_area_game_title').html('');
+            } else{
+                $('#alert_area_game_title').html('<div class="alert alert-danger alert-dismissable">\
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>\
+                    Please insert a Game Title.\
+                </div>');
+                bSuccess = false;
+            }
+
+            if (user_role != -1) {
+                $('#alert_area_game_position').html('');
+            } else{
+                $('#alert_area_game_position').html('<div class="alert alert-danger alert-dismissable">\
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>\
+                    Please choose a game position.\
+                </div>');
+                bSuccess = false;
+            }
+
+            $('#alert_area_oppo_email').html('');
+            $('#invite_modal').modal('hide');
+            if(bSuccess)
+                alert('ok');
+        }
+        else{
+            $('#alert_area_oppo_email').html('<div class="alert alert-danger alert-dismissable">\
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>\
+                Please insert a vaild email address.\
+            </div>');
+            return;
+        }
     })
 });
 
