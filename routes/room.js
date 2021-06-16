610e1120b80ac223498a4dc6fa276d37ca18dc7b
tuktarov9898 <84121020+Maz-POTAT@users.noreply.github.com>
2021-06-16 16:09:47 +0900

    Update room.js

--- routes/room.js
+++ routes/room.js
@@ -6,9 +6,9 @@ router.post("/create_game", roomController.postCreateRoom);
 router.post("/create_room", roomController.postCreateRandomRoom);
 router.post("/submit_turn", roomController.postSubmitTurn);
 
-router.get("/start_game",
+router.get("/start_game", (req, res, next) => {
    res.render("start_game", { title: 'Start Game',    active_page: 'start',  my_email: req.cookies.email})
-);
+});
 
 router.get("/game", roomController.getGame);
 router.get("/my_games", roomController.getMyGame);
