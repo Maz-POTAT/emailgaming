6156d5cd1a34fb39e8312eda1d211448fdbba314
tuktarov9898 <84121020+Maz-POTAT@users.noreply.github.com>
2021-06-23 07:37:11 +0900

    Update rooms.js

--- model/rooms.js
+++ model/rooms.js
@@ -13,10 +13,10 @@ const Room = sequelize.define("room", {
   },
   game_id: {
     type: Sequelize.INTEGER,
-    references:{
-      model: Game,
-      key: 'id',
-    }
+    // references:{
+    //   model: Game,
+    //   key: 'game_id',
+    // }
   },
   game_title: {
     type: Sequelize.STRING,
@@ -27,24 +27,24 @@ const Room = sequelize.define("room", {
   },
   player1_id: {
     type: Sequelize.INTEGER,
-    references:{
-      model: User,
-      key: 'id',
-    }
+    // references:{
+    //   model: User,
+    //   key: 'player1_id',
+    // }
   },
   player2_id: {
     type: Sequelize.INTEGER,
-    references:{
-      model: User,
-      key: 'id',
-    }
+    // references:{
+    //   model: User,
+    //   key: 'player2_id',
+    // }
   },
   status: Sequelize.INTEGER,
   data: Sequelize.STRING,
 });
 
-// Room.belongsTo(sequelize.models.user, {foreignKey: 'player1_id', as: 'Player1', constraints: false});
-// Room.belongsTo(sequelize.models.user, {foreignKey: 'player2_id', as: 'Player2', constraints: false});
-// Room.belongsTo(sequelize.models.game, {foreignKey: 'game_id', as: 'Game'});
+Room.belongsTo(sequelize.models.user, {foreignKey: 'player1_id', as: 'Player1', constraints: false});
+Room.belongsTo(sequelize.models.user, {foreignKey: 'player2_id', as: 'Player2', constraints: false});
+Room.belongsTo(sequelize.models.game, {foreignKey: 'game_id', as: 'Game'});
 
 module.exports = Room;
