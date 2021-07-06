6036acc940c9ea084460099331d4a97458fef921
tuktarov9898 <84121020+Maz-POTAT@users.noreply.github.com>
2021-07-06 15:11:32 +0900

    update profile

--- model/users.js
+++ model/users.js
@@ -12,6 +12,7 @@ const User = sequelize.define("user", {
   email: Sequelize.STRING,
   firstname: Sequelize.STRING,
   lastname: Sequelize.STRING,
+  notification: Sequelize.INTEGER,
   password: Sequelize.STRING,
 });
 
